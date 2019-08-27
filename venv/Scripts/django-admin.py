f41495b20b489471e1894a626b47c57f0ec0d6fd
Abmael Bandeira <abmaelbandeira@hotmail.com>
2019-08-27 00:24:34 -0300

    autenticação do admin

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\abmae\onedrive\área de trabalho\meuprojeto\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
